e7a3b433c67d5a6215ec52d0e6f2b86d9c0606ad
Anna <annapuchkova1993@gmail.com>
2026-05-16 13:03:08 +0300

    delete

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,21 +1,38 @@
+import { QueryClient, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Post } from "../../types/post";
 import css from "./PostList.module.css";
+import { deletePost } from "../../services/postService";
+import toast from "react-hot-toast";
 
 interface PostsListProps {
-posts: Post[]
+  posts: Post[];
 }
 
-export default function PostList({posts}: PostsListProps) {
+export default function PostList({ posts }: PostsListProps) {
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: deletePost,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["posts"],
+      });
+      toast.success("Post deleted successfully");
+    },
+  });
   return (
     <ul className={css.list}>
-      {posts.map(post => (<li key={post.id} className={css.listItem}>
-        <h2 className={css.title}>{post.title}</h2>
-        <p className={css.content}>{post.body}</p>
-        <div className={css.footer}>
-          <button className={css.edit}>Edit</button>
-          <button className={css.delete}>Delete</button>
-        </div>
-      </li>))}
+      {posts.map((post) => (
+        <li key={post.id} className={css.listItem}>
+          <h2 className={css.title}>{post.title}</h2>
+          <p className={css.content}>{post.body}</p>
+          <div className={css.footer}>
+            <button className={css.edit}>Edit</button>
+            <button onClick={() => mutation.mutate(post.id)} className={css.delete}>
+              Delete
+            </button>
+          </div>
+        </li>
+      ))}
     </ul>
   );
 }
--- src/services/postService.ts
+++ src/services/postService.ts
@@ -3,7 +3,10 @@ import { Post } from "../types/post";
 
 axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
 
-export const fetchPosts = async (searchText: string, page: number): Promise<{posts:Post[], totalCount: number}> => {
+export const fetchPosts = async (
+  searchText: string,
+  page: number
+): Promise<{ posts: Post[]; totalCount: number }> => {
   const res = await axios.get<Post[]>("/posts", {
     params: {
       q: searchText,
@@ -11,13 +14,16 @@ export const fetchPosts = async (searchText: string, page: number): Promise<{pos
       _limit: 8,
     },
   });
-    const totalCount = res.headers['x-total-count']
+  const totalCount = res.headers["x-total-count"];
 
-  return {posts:res.data, totalCount};
+  return { posts: res.data, totalCount };
 };
 
 // export const createPost = async (newPost) => {};
 
 // export const editPost = async (newDataPost) => {};
 
-// export const deletePost = async (postId) => {};
+export const deletePost = async (postId: number): Promise<Post> => {
+  const { data } = await axios.delete<Post>(`/posts/${postId}`);
+  return data;
+};
