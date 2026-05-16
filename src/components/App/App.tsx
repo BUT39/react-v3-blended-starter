a01d907eeb0a442034734b39faf37f0f339bc8e1
Dmytro But <butdmitrij25@mail.com>
2026-05-16 11:31:05 +0300

    update fetchPosts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,8 +4,20 @@ import SearchBox from "../SearchBox/SearchBox";
 import Pagination from "../Pagination/Pagination";
 
 import css from "./App.module.css";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { fetchPosts } from "../../services/postService";
+import { useState } from "react";
 
 export default function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState("");
+  const { data } = useQuery({
+    queryKey: ["posts", searchQuery, currentPage],
+    queryFn: () => fetchPosts(searchQuery, currentPage),
+    placeholderData: keepPreviousData,
+  });
+  console.log(data);
+
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
@@ -13,7 +25,6 @@ export default function App() {
         <Pagination />
         <button className={css.button}>Create post</button>
       </header>
-      <Modal>{/* Передати через children компонент CreatePostForm або EditPostForm */}</Modal>
       <PostList />
     </div>
   );
